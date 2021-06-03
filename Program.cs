9d372e1a168acde6a68d391bf5c5f1d46f244147
udayrv <udayreddyv@usf.edu>
2021-06-03 15:54:27 -0400

    classes updated

--- Program.cs
+++ Program.cs
@@ -63,15 +63,15 @@ namespace Assignment1
             String word1 = Console.ReadLine();
             Console.WriteLine("Enter the Second Word:");
             String word2 = Console.ReadLine();
-            String merged = Program5.merge(word1, word2);
+            String merged = Question5.merge(word1, word2);
             Console.WriteLine("The Merged Sentence fromed by both words is {0}", merged);
 
             //Question 6
             Console.WriteLine("Q6: Enter the sentence to convert:");
             string sentence = Console.ReadLine();
-            string goatLatin = Program6.ToGoatLatin(sentence);
+            string goatLatin = Question6.ToGoatLatin(sentence);
             Console.WriteLine("Goat Latin for the Given Sentence is {0}", goatLatin);
-            Console.WriteLine();
+            Console.WriteLine();    
 
         }
     }
@@ -115,29 +115,31 @@ namespace Assignment1
     // We have learnt the basic program to control the moments of a Robot in a 2D plane. 
     // 
     public class Question2
-        //Logic:
-        // We iterate over all the characters of the string and count the single time used char's.
-        // if total is 26, then return ture if not false
+    //Logic:
+    // We iterate all the alphabatical characters over the given string and count the instances
+    // where the alphabatical character is equal to character in given string.
+    // (we are checking all the alphabatical characters are present in the given string or not)
+    // if count is equal to 26 return ture if not false.
     {
         public static bool CheckIfPangram(string str)
         {
             try
             {
-                bool[] isUsed = new bool[26];
-                int ai = (int)'a';
-                int total = 0;
+                string alpha = "abcdefghijklmnopqrstuvwxyz"; 
+                int count = 0;
 
-                for (CharEnumerator en = str.ToLower().GetEnumerator(); en.MoveNext();)
+                foreach (char c in alpha)
                 {
-                    int d = (int)en.Current - ai;
-                    if (d >= 0 && d < 26)
-                        if (!isUsed[d])
+                    foreach (char i in str.ToLower())
+                    {
+                        if (c == i)
                         {
-                            isUsed[d] = true;
-                            total++;
+                            count++;
+                            break;
                         }
+                    }
                 }
-                return (total == 26);
+                return (count == 26);
             }
 
             catch (Exception)
@@ -160,19 +162,17 @@ namespace Assignment1
         {
             try
             {
-                int ans = 0;
+                int finalCount = 0;
 
-                for (int i = 0; i < nums.Length - 1; i++)
+                for (var i = 0; i < nums.Length; i++)
                 {
-                    for (int j = i + 1; j < nums.Length; j++)
-                    {
-                        if (nums[i] == nums[j])
-                        {
-                            ans++;
-                        }
-                    }
+                    for (var j = 0; j < nums.Length; j++)
+
+                        if (nums[i].Equals(nums[j]) && i < j)
+
+                            finalCount++;
                 }
-                return ans;
+                return finalCount;
             }
             catch (Exception)
             {
@@ -187,28 +187,27 @@ namespace Assignment1
     //
     class Question4
     // Logic:
-    // First we find the cummulative sum of the given array and check the pivot index by iterating over that array.
+    // We first consider the leftsum = 0 and sum as total sum of elements in given array.
+    // then, we remove the first element from the total sum and check the sum of the remaining elements is equal to the first element. 
+    // If true we return the index of the element.
+    // If not we repeat for remaining elements in the array by increasing the leftsum by next element value.
     {
         public static int PivotIndex(int[] nums)
         {
             try
             {
-                int len = nums.Length;
-                int sum = 0;
-                int[] prefix = new int[len];
-                for (int i = 0; i < len; i++)
-                {
-                    prefix[i] = sum;
-                    sum += nums[i];
-                }
+                if(nums == null || nums.Length == 0)
+                return -1;
 
+                int sum = nums.Sum();
+                int leftsum = 0;
 
-                for (int i = 0; i < len; i++)
+                for (int i = 0; i < nums.Length; i++)
                 {
-                    if (prefix[i] == prefix[len - 1] - prefix[i] - nums[i] + nums[len - 1])
-                    {
+                    if ((sum = (sum - nums[i])) == leftsum)
                         return i;
-                    }
+                    else
+                        leftsum += nums[i];
                 }
 
                 return -1;
@@ -224,33 +223,27 @@ namespace Assignment1
     }
     // Self Reflection:
     //
-    // Time complexity is O(N)
+    // Time complexity is O(N2)
     // We have learnt some advance techniques of working on array and it's indexes. 
     //
-    class Program5
+    class Question5
         // Logic:
-        // We iterate the given strings with characters and merge the characters alternatively to our result.
-        // If there are any additional characters, we simply append at the end of the result.
+        // We use the StringBuilder class for our solution.
+        // We iterate over both the strings and append each character from the string 
+        // alternatively to our StringBuilder. Finally we convert it to the string.
     {
         public static string merge(string word1, string word2)
         {
             try
             {
-                string result = "";
+                var bldr = new StringBuilder();
 
-
-                for (int i = 0; i < word1.Length || i < word2.Length; i++)
+                for (int i = 0; i < Math.Max(word1.Length, word2.Length); i++)
                 {
-
-
-                    if (i < word1.Length)
-                        result += word1[i];
-
-
-                    if (i < word2.Length)
-                        result += word2[i];
+                    if (i < word1.Length) bldr.Append(word1[i]);
+                    if (i < word2.Length) bldr.Append(word2[i]);
                 }
-                return result;
+                return bldr.ToString();
             }
             catch (Exception e)
             {
@@ -263,37 +256,41 @@ namespace Assignment1
     // Self Reflection:
     //
     // Time complexity is O(N)
-    // We have learnt how to work with concatination of arrays and deal with multiple char arrays.
+    // We have learnt to work with StringBuilder class and how to work with strings accourding to their lengths.
     //
-    class Program6
-        // Logic:
-        // we divide the given scentence on white spaces and iterate over all the words in a scentence.
-        // Change the words in a string according to the conditions given for "Goat Latin" language and finally merge all the words.
-        // 
+    class Question6
+    // Logic:
+    // first we split the given sentence into words and then filter accourding to the first letter of the word.
+    // if it is an vowel we append 'ma' and 'a' to the end of the word as per it's index.
+    // if it is a consonant we remove the first letter and append it to the end and add 'ma' to the end,
+    // and add 'a' to the end of the word as per it's index.
     {
         public static string ToGoatLatin(string sentence)
         {
             try
             {
-                var words = sentence.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
-                string suffix = "a", result = "";
-                var vowels = new List<char>() { 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u' };
-                foreach (var word in words)
+                var words = sentence.Split();
+                var sb = new StringBuilder();
+                for (int i = 0; i < words.Length; i++)
                 {
-                    if (vowels.Any(x => x == word[0]))
+                    var w = words[i];
+                    if (i > 0)
                     {
-                        result += word + "ma" + suffix + " ";
+                        sb.Append(' ');
+                    }
+                    if ("aeiouAEIOU".Contains(w[0]))
+                    {
+                        sb.Append(w);
                     }
                     else
                     {
-                        result += word.Substring(1) + word[0] + "ma" + suffix + " ";
+                        sb.Append(w.Substring(1));
+                        sb.Append(w[0]);
                     }
-
-                    suffix += "a";
-
+                    sb.Append("ma");
+                    sb.Append('a', i);
                 }
-
-                return result.Substring(0, result.Length - 1);
+                return sb.ToString();
             }
             catch (Exception)
             {
@@ -303,7 +300,7 @@ namespace Assignment1
     }
     // Self Reflection:
     //
-    // Time complexity is O(N)
+    // Time complexity is O(N2)
     // We have learnt some moderate level of string manipulation on string arrays. 
     // We have seen cooncatination of strigs and filtering of words according to it's first letter.
     //
